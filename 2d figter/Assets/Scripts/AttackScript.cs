2d90d24df47a78fdbdff9a71eb591f97492d1602
Ramjar420 <61980781+Ramjar420@users.noreply.github.com>
2021-07-06 14:02:13 +0200

    Changed the attack script to work properly.
    +Added attack function.
    +Added function to see gizmos ranges.
    -Removed AttackPoint hitbox.

--- 2d figter/Assets/Scripts/AttackScript.cs
+++ 2d figter/Assets/Scripts/AttackScript.cs
@@ -7,67 +7,75 @@ public class AttackScript : MonoBehaviour
     
     public KeyCode attack;
     private Animator anim;
-    public Transform AttackPoint;
-    public float AttackRange =0.5f;
     private bool isAttacking;
     private bool isBlocking; 
     private float timeBtwAttack;
     public float startTimeBtwAttack;
 
+    public Transform attackPoint;
+    public float attackRange = 2f;
+    public LayerMask playerMask;
     
     void attackanim()
         {
             //play attack animation
             anim.SetTrigger("Attack");
         }
-    void OnTriggerStay2D(Collider2D other)
-    {    
-        if(isAttacking)
-        {
-            if(!isBlocking)
-            {
-                if(other.tag == "Player1")
-                {
-                    FindObjectOfType<GameManager>().HurtP1();
-                 }
-                 if(other.tag == "Player2")
-             {
-                FindObjectOfType<GameManager>().HurtP2();
-             }
-
-
-            }
-        }
-
-            
-
-        
-    }
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        attackPoint = this.gameObject.transform.GetChild(1);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(timeBtwAttack <= 0){
-                if(Input.GetKeyDown(attack)){
-                    isAttacking = true;
-                     attackanim();
-                     timeBtwAttack = startTimeBtwAttack;
-                 }
+            if(Input.GetKeyDown(attack))
+            {
+                Attack();
+                attackanim();
+                timeBtwAttack = startTimeBtwAttack;
+            }
         } else {
             timeBtwAttack -= Time.deltaTime;
         }
-        
-        if(Input.GetKeyUp(attack)){
-            isAttacking = false; 
+    }
+    void Attack()
+    {
+        //checkt de colliders van geraakte enemies. en slaat ze op in een array.
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerMask);
+
+        foreach (Collider2D Player in hitEnemies)
+        {
+            if (Player.CompareTag("Player2"))
+            {
+                if (!isBlocking)
+                {
+                    Debug.Log("Hit" + Player.name);
+                    FindObjectOfType<GameManager>().HurtP2();   
+                }
+            }
+            if (Player.CompareTag("Player1"))
+            {
+                if (!isBlocking)
+                {
+                    Debug.Log("Hit" + Player.name);
+                    FindObjectOfType<GameManager>().HurtP1();
+                }
+            }
         }
-        
-       
+    }
+    //deze functie is alleen om de range te zien van bijv de attackpoint range. deze kan dus weg als dit niet meer nodig is.
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
 
